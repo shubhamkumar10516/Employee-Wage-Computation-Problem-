2eac7b78b2c27c926beb66245f97ae1e5f1970c1
shubham <shubhamkumar10516@gmail.com>
2020-09-22 23:41:10 +0530

    [Shubham]ADDED: Wage calculating file using while loop added

--- Main.java
+++ Main.java
@@ -4,15 +4,18 @@ public class EmployeeWageBuilderFor{
   public static final int IS_PART_TIME = 1;
   public static final int IS_FULL_TIME = 2;
   public static final int RATE_PER_HOUR = 20;
-  public static final int NUM_OF_WORKING_DAYS = 20;
+  public static final int NUM_OF_WORKING_DAYS = 2;
+  public static final int MAX_HRS_IN_MONTH = 100;
   
   public static void main(String args[]){
 
 
-    int empHour =0;
+    int empHour =0, totalEmpHrs = 0, totalWorkingDays = 0;
     int empWage = 0, totalEmpWage = 0;
    
-    for(int i = 0 ; i < NUM_OF_WORKING_DAYS; i++){
+    while(empHour <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+           totalWorkingDays++;
+
            int empCheck = (int)Math.floor(Math.random()*10)%3;
            switch(empCheck) {
                 case IS_FULL_TIME:
@@ -25,11 +28,12 @@ public class EmployeeWageBuilderFor{
                    empHour = 0;
     }
 
-    empWage = empHour*RATE_PER_HOUR;
-    totalEmpWage += empWage; 
-     System.out.println(" daily employee Wage : "+" "+empWage);
+    
+    totalEmpHrs += empHour; 
+    System.out.println(" day no : "+totalWorkingDays +" Emp Hr: "+empHour);
   }
-     System.out.println(" total employee Wage : "+" "+totalEmpWage);
+    totalEmpWage = Rate_PER_HOUR * totalEmpHrs;
+     System.out.println(" total employee Wage : "+totalEmpWage);
      
   }
 

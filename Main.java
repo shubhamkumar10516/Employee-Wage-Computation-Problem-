736f93f6a52cdd1bef7538769534a5022a813c91
shubham <shubhamkumar10516@gmail.com>
2020-09-23 00:00:41 +0530

    [Shubham] UC2 Daily employee wage claculator file added

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ public class EmployeeWageBuilder1{
 
   public static void main(String args[]){
 
-    int IS_PART_TIME = 1;
-    int IS_FULL_TIME = 2;
+    int IS_PART_TIME = 0;
+    int IS_FULL_TIME = 1;
     int RATE_PER_HOUR = 20;
     int empHour =0;
     int empWage = 0;
-    double empCheck = Math.floor(Math.random()*10)%3;
+    double empCheck = Math.floor(Math.random()*10)%2;
 
     if(empCheck == IS_FULL_TIME)
       empHour = 8;

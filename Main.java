98164ae24eae03e9f690aa078a834da8da30237c
shubham <shubhamkumar10516@gmail.com>
2020-09-22 23:03:23 +0530

    [Shubham]ADDED: switch case included  file

--- Main.java
+++ Main.java
@@ -1,24 +1,31 @@
-public class EmployeeWageBuilder1{
+public class EmployeeBuilderSwitch{
+
+
+  public static final int IS_PART_TIME = 1;
+  public static final int IS_FULL_TIME = 2;
+  public static final int RATE_PER_HOUR = 20;
 
   public static void main(String args[]){
 
-     int IS_PART_TIME = 1;
-     int IS_FULL_TIME = 2;
-     int RATE_PER_HOUR = 20;
+
     int empHour =0;
     int empWage = 0;
-    double empCheck = Math.floor(Math.random()*10)%3;
+    int empCheck = (int)Math.floor(Math.random()*10)%3;
 
-    if(empCheck == IS_FULL_TIME)
-      empHour = 8;
-    else if(empCheck == IS_PART_TIME)
-      empHour = 4;
-    else
-     empHour = 0;
+    switch(empCheck) {
+      case IS_FULL_TIME:
+         empHour = 8;
+         break;
+      case IS_PART_TIME:
+         empHour = 4;
+         break;
+      default:
+        empHour = 0;
+    }
 
-   empWage = empHour*RATE_PER_HOUR;
+    empWage = empHour*RATE_PER_HOUR;
 
-  System.out.println("empWage : "+empWage);
+    System.out.println("empWage : "+empWage);
      
   }
 

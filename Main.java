81b0cecf3aa926de3d136c6f12ea19b236bd1406
shubham <shubhamkumar10516@gmail.com>
2020-09-22 22:07:52 +0530

    [Shubham]ADDED: employee daily wage calculator java file

--- Main.java
+++ Main.java
@@ -1,14 +1,25 @@
-public class EmployeeWageBuilder{
+public class EmployeeWageBuilder1{
 
   public static void main(String args[]){
 
-     int IS_FULL_TIME = 1;
-     double empCheck = Math.floor(Math.random()*10)%2;
+    int IS_PART_TIME = 1;
+    int IS_FULL_TIME = 2;
+    int RATE_PER_HOUR = 20;
+    int empHour =0;
+    int empWage = 0;
+    double empCheck = Math.floor(Math.random()*10)%3;
 
-     if(empCheck == IS_FULL_TIME)
-          System.out.println("Employee is Present");
-     else
-          System.out.println("Employee is Absent");
+    if(empCheck == IS_FULL_TIME)
+      empHour = 8;
+    else if(empCheck == IS_PART_TIME)
+      empHour = 4;
+    else
+     empHour = 0;
+
+    empWage = empHour*RATE_PER_HOUR;
+
+    System.out.println("empWage : "+empWage);
      
   }
 
+}

020843b76ef374b137dc9bf00dc6cd91f0a59c86
shubham <shubhamkumar10516@gmail.com>
2020-09-22 22:48:04 +0530

    [Shubham]ADDED: Full time and Part time both wage calculator file

--- Main.java
+++ Main.java
@@ -1,31 +1,24 @@
-public class EmployeeBuilderSwitch{
-
-
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int RATE_PER_HOUR = 20;
+public class EmployeeWageBuilder1{
 
   public static void main(String args[]){
 
-
+     int IS_PART_TIME = 1;
+     int IS_FULL_TIME = 2;
+     int RATE_PER_HOUR = 20;
     int empHour =0;
     int empWage = 0;
-    int empCheck = (int)Math.floor(Math.random()*10)%3;
+    double empCheck = Math.floor(Math.random()*10)%3;
 
-    switch(empCheck) {
-      case IS_FULL_TIME:
+    if(empCheck == IS_FULL_TIME)
       empHour = 8;
-      break;
-    case IS_PART_TIME:
+    else if(empCheck == IS_PART_TIME)
       empHour = 4;
-      break;
-    default:
+    else
      empHour = 0;
-    }
 
-    empWage = empHour*RATE_PER_HOUR;
+   empWage = empHour*RATE_PER_HOUR;
 
-    System.out.println("empWage : "+empWage);
+  System.out.println("empWage : "+empWage);
      
   }
 
